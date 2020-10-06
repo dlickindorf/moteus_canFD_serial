62812beb892ea1365504252f1de518f1ee698f7e
Damian Lickindorf <d.lickindorf@gmail.com>
2020-10-06 22:21:36 +0200

    venv and .idea deleated

--- venv/lib/python3.6/site-packages/serial/abc/__init__.py
+++
@@ -1,9 +0,0 @@
-# region Backwards Compatibility
-from __future__ import nested_scopes, generators, division, absolute_import, with_statement, \
-   print_function, unicode_literals
-from ..utilities.compatibility import backport
-
-backport()
-
-from . import model, properties
-
